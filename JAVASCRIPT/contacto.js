0fc5597ca18f480ac8f5d0dee9e28563b78fe52c
luciana rossa <lucianarossa@gmail.com>
2022-05-30 15:46:55 -0300

    actualizacion contacto

---
+++ JAVASCRIPT/contacto.js
@@ -0,0 +1,60 @@
+let comentario=document.getElementById("comentarios")
+let templateHTML=""
+function mostrarcontacto(){
+    
+    templateHTML+=`
+    <div class="formulario">
+        <form action="" method="get">
+            <h1>Contacto</h1>
+            <fieldset>
+            <label for="Nombre:">Nombre:</label>
+            <input type="text" size="32" name="Nombre" id="Nombre:">
+        </fieldset>
+        <fieldset>
+            <label for="Apellido:">Apellido:</label>
+            <input type="text" size="32" name="Apellido" id="Apellido:">
+        </fieldset>
+        <fieldset>
+            <label for="Telefono:">Telefono:</label>
+            <input type="text" size="32" name="Telefono" id="Telefono:">
+            </fieldset>
+            <fieldset class="mover-radio">
+            <p>¿Que mascota tienes?</p>
+                    <select name="" id="">
+                        <option value="">¡Selecciona tu mascota!</option>
+                        <option value="">Perro</option>
+                        <option value="">Gato</option>
+                        <option value="">Conejo</option>
+                        <option value="">Otros...</option>
+                    </select>
+        </fieldset>
+        <legend>Comentarios</legend>
+        <textarea name="Comentarios" id="comentarios" cols="45" rows="5"></textarea>
+    <fieldset>
+        <input type="submit" id="comentarios-submit">
+    </fieldset>
+        </form>
+    </div>
+<div class="d-flex justify-content-center">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.6871004428513!2d-58.432572384236636!3d-34.6120729654484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca429815227f%3A0x5d302e497c2cda87!2sR%C3%ADo%20de%20Janeiro%20300%2C%20C1405CCB%20CABA%2C%20Argentina!5e0!3m2!1ses-419!2scl!4v1653931633998!5m2!1ses-419!2scl" width="500" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>    
+</div>
+    `
+    document.querySelector("#contacto").innerHTML=templateHTML
+}
+mostrarcontacto()
+
+
+
+
+
+
+let enviar = document.getElementById("comentarios-submit")
+enviar.addEventListener("click", (event) => {
+    event.preventDefault()
+    Swal.fire(
+        'Muchas gracias por escribirnos,',
+        'Ya hemos enviado tu información.',
+        'success'
+      )
+    })
+    
